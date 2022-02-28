73465b1204b049f947dda081a13464607244238b
asliddin03 <asliddin03@users.noreply.github.com>
2022-02-28 17:58:58 +0300

    added comments to run.sh

--- task1/run.sh
+++ task1/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# объявляем список из 4 аргумента
 ARGUMENT_LIST=(
   "input_folder"
   "extension"
@@ -7,18 +8,11 @@ ARGUMENT_LIST=(
   "backup_archive_name"
 )
 
-
-# read arguments
-opts=$(getopt \
-  --longoptions "$(printf "%s:," "${ARGUMENT_LIST[@]}")" \
-  --name "$(basename "$0")" \
-  --options "" \
-  -- "$@"
-)
-
-eval set --$opts
-
+# $# - переменная аргументы
+# в цикле мы рассмотриваем пока значение $# > 0
 while [[ $# -gt 0 ]]; do
+  # рaссмотриваем аргумент $1 в switchcase в каждом из случаев заносим значение аргумента $2 в соответствующую переменную 
+  # shift сдвигает все аргументы на две позиции в лево
   case "$1" in
     --input_folder)
       input_folder=$2
@@ -30,6 +24,7 @@ while [[ $# -gt 0 ]]; do
       shift 2
       ;;
 
+# $
     --backup_folder)
       backup_folder=$2
       shift 2
@@ -48,8 +43,13 @@ while [[ $# -gt 0 ]]; do
 done
 
 mkdir -p $backup_folder
-find "$input_folder" -name "*.$extension" -print0 | 
+# мы ищем все файлы с росширением extension
+# print0 что бы функция find ничего не выводила
+find "$input_folder" -name "*.$extension" -print0 |
+# IFS - форматирует вывод функции find и все отделные названия файлов итарируются в цикле 
 while IFS= read -r -d '' file; do
+    # cp - копирует файл в папку назначения
+    # --backup=numbered - нумирует файл с одинаковую названия
     cp --backup=numbered "$file" "$backup_folder"
 done 
 
