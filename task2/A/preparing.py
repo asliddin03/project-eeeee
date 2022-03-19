24646aeed65a3197f673c0308a2f1516ed4d2856
asliddin03 <asliddin03@users.noreply.github.com>
2022-03-19 05:14:47 +0300

    first commit

---
+++ task2/A/preparing.py
@@ -0,0 +1,3 @@
+my_file = open("index.h", "w+")
+my_file.write("int sum(int a, int b) { return a + b;}") 
+my_file.close()
---
+++ task2/C/main_test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "A/index.h"
+#include "B/lib.h"
+
+TEST (tast_index, tast1) {
+    ASSERT_EQ(sum(2, 2), 4); }
+TEST (tast_lib, tast2) {
+    ASSERT_EQ(mult(4, 2), 8);
+    ASSERT_EQ(mult(7, 7), 49);
+}
