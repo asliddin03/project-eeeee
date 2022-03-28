d813336dd89535c789f42ea9a4f11346fa23fe6f
asliddin03 <asliddin03@users.noreply.github.com>
2022-03-28 03:02:10 +0300

    sum changed to inline func

--- task2/A/preparing.py
+++ task2/A/preparing.py
@@ -1,3 +1,3 @@
 my_file = open("index.h", "w+")
-my_file.write("int sum2_2(){ return 4; }") 
+my_file.write("inline int sum2_2(){ return 4; }") 
 my_file.close()
--- task2/C/main_test.cpp
+++ task2/C/main_test.cpp
@@ -3,7 +3,7 @@
 #include "B/lib.h"
 
 TEST (tast_index, tast1) {
-    ASSERT_EQ(sum2_2(),4); }
+    ASSERT_EQ(sum2_2(), 4); }
 TEST (tast_lib, tast2) {
     ASSERT_EQ(mult(4, 2), 8);
     ASSERT_EQ(mult(7, 7), 49);
