60a77d5b58a407e8cfa7caf53446fdc6688abb11
asliddin03 <asliddin03@users.noreply.github.com>
2022-04-13 22:14:59 +0300

    task2 fixed

---
+++ task2/build/CMakeFiles/CTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/CTest"
+  "../bin/CTest.pdb"
+  "CMakeFiles/CTest.dir/C/main_test.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
