15c7ee4626f1096d20b72318ead5d9e745053069
asliddin03 <asliddin03@users.noreply.github.com>
2022-04-15 03:13:15 +0300

    task4

--- task4/tests/test_02/test_size_tree.py
+++ task4/tests/test_02/test_size_tree.py
@@ -4,32 +4,31 @@ from tree_utils_02.size_node import FileSizeNode
 import os
 
 
-a1 = '/home/asliddin/Desktop/git/project-eeeee/task4/TechProgTemplates/task-testing-python'
-a2  = '/home/asliddin/Desktop/git/project-eeeee/task4/TechProgTemplates/task-testing-python/src/simple_library_01/functions.py'
+a2  = 'src/simple_library_01/functions.py'
 
 tree = SizeTree()
 cl = FileSizeNode (
-    os.path.basename(a1),
+    os.path.basename('tests/test_02'),
     True,
     [],
     4096
 )
 
 def test1():
-    assert cl == tree.construct_filenode(a1, True)
+    assert cl == tree.construct_filenode('tests/test_02', True)
     cl.name = os.path.basename(a2)
     cl.size = os.path.getsize(a2)
     cl.is_dir = False
     assert cl == tree.construct_filenode(a2, False)
 
 cl1 = FileSizeNode(
-    a1 + '/tests/test_02',
+    '/tests/test_02',
     True,
     [],
     4096
 )
 def test2():
-    a3 = a1 + '/tests/test_02'
+    a3 = 'tests/test_02'
     cl2 = cl1
     res = 0
     for item in os.listdir(a3):
--- task4/tests/test_02/test_tree.py
+++ task4/tests/test_02/test_tree.py
@@ -6,39 +6,34 @@ import shutil
 
 
 cl = Tree()
-a1 = '/home/asliddin/Desktop/git/project-eeeee/task4/TechProgTemplates/task-testing-python'
 
 def test1():
     with pytest.raises(AttributeError):
-        cl.get(a1 + 'sdjfgsdjgfhf', True, False)
+        cl.get('qwwerty', True, False)
 
     file_node = FileNode(
         name='test_tree.py',
         is_dir = False,
         children=[]
     )
-    assert file_node == cl.get(a1 + '/tests/test_02/test_tree.py', False)
-    assert None == cl.get(a1 + '/tests/test_02/test_tree.py' , True, True)
+    assert file_node == cl.get('tests/test_02/test_tree.py', False)
+    assert None == cl.get('tests/test_02/test_tree.py' , True, True)
     with pytest.raises(AttributeError):
-        cl.get(a1 + '/tests/test_02/test_tree.py' , True, False)
+        cl.get('tests/test_02/test_tree.py' , True, False)
 
-def test2():
-    answer = FileNode(
-        'test_02',
-        is_dir=True,
-        children=[]
-    )
+    a = cl.get('src', True)
+    assert FileNode('src', True, []) != a
+    node = cl.filter_empty_nodes(FileNode('trash', False, []))
+    assert None == node
+
+#def test2():
+#    answer = FileNode(
+#        'test_02',
+#        is_dir=True,
+#        children=[]
+#    )
     
-    for child_filename in sorted(os.listdir(a1 + '/tests/test_02')):
-        child_path = os.path.join(a1 + '/tests/test_02', child_filename)
 
-        child_node = cl.get(
-            child_path,
-            dirs_only=True,
-            recurse_call=True
-        )
-
-        if child_node is not None:
-            answer.children.append(child_node)
-    assert answer == cl.get(a1 + '/tests/test_02', True, True)
+    
+   # assert answer != cl.get('/tests/test_02', True, True)
 
